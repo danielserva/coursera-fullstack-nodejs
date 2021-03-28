364bc81a16f6c978dac7c18e97b1c10abfdb0b6f
daniel <danielserva@gmail.com>
2021-03-28 01:18:31 +0100

    Mongoose

---
+++ node-mongoose/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb+srv://admin:Sopinh%401@cluster0.lfgfq.mongodb.net/';
+const connect = mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true } );
+
+connect.then((db) => {
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true,
+            useFindAndModify : false
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+});
