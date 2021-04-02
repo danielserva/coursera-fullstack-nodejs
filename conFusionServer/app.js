91b45076d32efb8d186ad480e30c3f30514a045e
daniel <danielserva@gmail.com>
2021-04-02 18:56:01 +0200

    Express Session

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,8 +1,10 @@
 var createError  = require('http-errors');
 var express      = require('express');
 var path         = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger       = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 const mongoose   = require('mongoose');
 
 var indexRouter    = require('./routes/index');
@@ -10,7 +12,7 @@ var usersRouter    = require('./routes/users');
 const dishRouter   = require('./routes/dishRouter');
 const promoRouter  = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
-const Dishes       = require('./models/dishes');
+// const Dishes       = require('./models/dishes');
 
 // const dbname = 'coursera-nodejs';
 const url = 'mongodb+srv://admin:Sopinh%401@cluster0.lfgfq.mongodb.net/coursera-nodejs';
@@ -29,41 +31,49 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth (req, res, next) {
-  console.log(req.headers);
-  if (!req.signedCookies.user) {
-      
+  console.log(req.session);
+
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
+      res.setHeader('WWW-Authenticate', 'Basic');                        
       err.status = 401;
       next(err);
       return;
     }
-
     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-      res.cookie('user','admin',{signed: true});  
+      req.session.user = 'admin';
       next(); // authorized
     } else {
       var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
+      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
     }
-  } else {
-    if (req.signedCookies.user === 'admin') {
+  }
+  else {
+    if (req.session.user === 'admin') {
+      console.log('req.session: ',req.session);
       next();
-    } else {
+    }
+    else {
       var err = new Error('You are not authenticated!');
       err.status = 401;
       next(err);
-    } 
+    }
   }
 }
 
